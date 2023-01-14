ecaaf6110423d79829b659cd160be8f4af4074ec
Aqib Siddiqui <aqibsid007007@gmail.com>
2023-01-14 10:27:32 +0530

    add
    code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import  { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+
+const App = () => {
+  return (
+
+    <ChatEngine
+
+    height="100vh"
+    projectId="82d86eca-c507-407f-a180-6f21869fbf3c"
+    userName="Aqib"
+    userSecret="2ca5bd41-efcc-4b15-9c94-ab07affd0841"
+
+    />
+  );
+}
+export default App;
